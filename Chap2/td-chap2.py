25b1f28393bfecbcc9af23878e00fabc1cc7fc22
tinou_99 <tinou99.f@gmail.com>
2019-09-26 11:03:30 +0200

    Update td-chap2.py

--- Chap2/td-chap2.py
+++ Chap2/td-chap2.py
@@ -60,5 +60,32 @@ plt.plot(x,y) #droite sans point
 #plt.squatter(x,y) nuage de points 
 
 
+#%%
+#loi normale 
+from random import normalvariate
+
+y_normal = [normalvariate(0,1) for i in range(10000)]
+plt.hist(y_normal, bins=50, normed=True)
+plt.show()
+
+
+#%%
+
+import numpy as np
+from random import random
+import matplotlib.pyplot as plt
+
+#np.random.normal(loc=0.0, scale=1.0, size=None)
+
+
+#display the histogram of the samples, along with the probability density function
+mu, sigma = 0, 0.1 # mean and standard deviation
+s = np.random.normal(mu, sigma, 1000)
+
+count, bins, ignored = plt.hist(s, 30, normed=True)
+plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) * np.exp( - (bins - mu)**2 / (2 * sigma**2)), linewidth=2, color='r')
+plt.show()
+
+
 
 
