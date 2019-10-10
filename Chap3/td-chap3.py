d4d2fbc02ff1f092b223653015c0eca92d9c2eab
tinou_99 <tinou99.f@gmail.com>
2019-10-10 06:11:12 +0200

    Updates

---
+++ Chap3/td-chap3.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  1 16:55:18 2019
+
+@author: tinou
+"""
+import numpy as np
+
+
+def moyenne(l):
+    s = 0
+    for i in range(len(l)):
+        s += l[i]
+    return (s/len(l))
+
+def numpy_moyenne(l):
+    return np.mean(l)
+   
+    
+
+
+def produit_scalaire(u,v):
+    return sum([x * y for x, y in zip(u, v)])
+
+def numpy_produit_scalaire(u,v):
+    return np.dot(u,v)
+
+
+
+def variance(a):
+    m = moyenne(a)
+    v = 0
+    for i in (len(a)):
+        v += (a[i]-m)**2
+    return (v/len(a))
+
+def nympy_variance(a):
+    return np.var(a)
+
+
+
+
+ 
