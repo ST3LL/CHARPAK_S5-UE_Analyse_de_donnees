31e298c4ff3132175b2752ce9a524b8e5e90393e
tinou99 <tinou99.f@gmail.com>
2019-11-08 14:37:07 +0100

    Update td-chap3.py

--- Chap3/td-chap3.py
+++ Chap3/td-chap3.py
@@ -6,7 +6,7 @@ Created on Tue Oct  1 16:55:18 2019
 """
 import numpy as np
 
-
+#%%
 def moyenne(l):
     s = 0
     for i in range(len(l)):
@@ -18,14 +18,13 @@ def numpy_moyenne(l):
    
     
 
-
-    
+#%%   
 def nympy_ecarttype(x):
     return np.std(x)
 
 
 
-
+#%%
 def produit_scalaire(u,v):
     return sum([x * y for x, y in zip(u, v)])
 
@@ -33,7 +32,7 @@ def numpy_produit_scalaire(u,v):
     return np.dot(u,v)
 
 
-
+#%%
 def variance(a):
     m = moyenne(a)
     v = 0
@@ -43,6 +42,16 @@ def variance(a):
 
 def nympy_variance(a):
     return np.var(a)
+
+
+#%%
+def covariance(x):
+    return np.cov(x)
+
+
+#%%
+def linear(x, params=(0,1)):
+    return params[1]*x + params[0] + np.random.normal(size=len(x))
 
 
 
